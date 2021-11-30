f15f8a098165894959e1edaa6e355efb1eba0962
Febzey <braydent1235@gmail.com>
2021-11-30 07:03:18 -0600

    Changes some formatting

--- src/index.ts
+++ src/index.ts
@@ -12,25 +12,25 @@ import connect from './database/createpool.js';
 import playtime from './functions/TrackPlaytime.js';
 
 export let bot: any;
-export let database:any = false;
-export let querys:any;
+export let database: any = false;
+export let querys: any;
 
-export const Info = (text: string) => console.log('\x1b[34m%s\x1b[0m',`${text}`);
-export const Success = (text: string) => console.log('\x1b[32m%s\x1b[0m',`${text}`);
+export const Info = (text: string) => console.log('\x1b[34m%s\x1b[0m', `${text}`);
+export const Success = (text: string) => console.log('\x1b[32m%s\x1b[0m', `${text}`);
 export const Fail = (text: string) => console.error('\x1b[31m%s\x1b[0m', `${text}`);
 export let channels: string[] = [];
-export const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+export const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 function loadChannels() {
-    if(!database) return;
+    if (!database) return;
     database.query(`SELECT * FROM livechats`, (err: unknown, res: Array<RowDataPacketLiveChat>) => {
         if (err) return Fail("Error loading livechat channels."), console.error(err);
         channels = [];
         res.forEach((element) => {
-            channels.indexOf(element.channelID) === -1 || null 
-                    ? channels.push(element.channelID)
-                    : 0;
-            
+            channels.indexOf(element.channelID) === -1 || null
+                ? channels.push(element.channelID)
+                : 0;
+
         });
     })
 
@@ -46,21 +46,23 @@ function loadChannels() {
          * config options are present.
          * also getting our main options.
          */
-        const _env:any = await Check();
+        const _env: any = await Check();
         if (_env) Success("Checks where a success.");
-        
-        const bot_config:any = _env.bot_config;
+
+        const bot_config: any = _env.bot_config;
         querys = _env.querys;
 
         /**
          * Creating connection the database.
          */
         if (_env.USE_DATABASE) {
-            
-            database = await connect(_env.DATABASE_HOST,
-                                     _env.DATABASE_USER,
-                                     _env.DATABASE_PASS,
-                                     _env.DATABASE);
+
+            database = await connect(
+                _env.DATABASE_HOST,
+                _env.DATABASE_USER,
+                _env.DATABASE_PASS,
+                _env.DATABASE
+            );
 
             if (database) Success("Connected to database successfully.");
             if (!database) Fail("Database connection was NOT successful.");
@@ -72,20 +74,20 @@ function loadChannels() {
          */
         if (_env.USE_DISCORD) {
             client.login(_env.DISCORD_TOKEN);
-            
+
             client.once("ready", () => {
-                
+
                 Success("Discord bot successfully logged in.");
                 loadChannels();
                 setInterval(() => { loadChannels() }, bot_config.channelRefreshTime);
 
             });
 
-            client.on("messageCreate", (message:any) => {
+            client.on("messageCreate", (message: any) => {
 
                 const { content, channel, author, member } = message;
                 if (author.id === client.user.id) return;
-                
+
                 try {
 
                     if (!channels || channels.length <= 0 || !database) return;
@@ -97,7 +99,7 @@ function loadChannels() {
 
                 }
 
-                catch(error) {
+                catch (error) {
                     return console.error(error);
                 }
 
@@ -108,50 +110,52 @@ function loadChannels() {
         /**
          * Logging in the Mineflayer Bot.
          */
-        if (_env.USE_MINEFLAYER) {
+        if (!_env.USE_MINEFLAYER) return;
 
-            bot = await startBot(_env.MC_HOST,
-                                 _env.MC_USER, 
-                                 _env.MC_PASS, 
-                                 _env.MC_VERSION, 
-                                 parseInt(_env.MC_PORT),
-                                 _env.bot_config.RelayChannel
-                                 );
 
-            /**
-             * Loading custom chat patterns. and tablist.
-             */
-            patterns(bot);
+        bot = await startBot(
+            _env.MC_HOST,
+            _env.MC_USER,
+            _env.MC_PASS,
+            _env.MC_VERSION,
+            parseInt(_env.MC_PORT),
+            _env.bot_config.RelayChannel
+        );
 
-            /**
-             * Loading tablist creator and playtime counter.
-             * if database connection was a success.
-             */
-            if (database) {
-                tab(bot,database,querys);
-                setInterval(() => { playtime(bot) }, 60000);
-            };
+        /**
+         * Loading custom chat patterns. and tablist.
+         */
+        patterns(bot);
 
-            /**
-             * Handling Mineflayer Events.
-             */
-            const eventDir:string[] = (await readdir('./dist/events')).filter(file=>file.endsWith('.js'));
-
-            await handleEvents(bot, 
-                               eventDir, 
-                               bot_config, 
-                               database, 
-                               querys);
-            
-            Success("Events active.");
-
-            /**
-             * Loading commands.
-             */
-            await loadCommands();
-            Success("Commands loaded.");
-
+        /**
+         * Loading tablist creator and playtime counter.
+         * if database connection was a success.
+         */
+        if (database) {
+            tab(bot, database, querys);
+            setInterval(() => { playtime(bot) }, 60000);
         };
+
+        /**
+         * Handling Mineflayer Events.
+         */
+        const eventDir: string[] = (await readdir('./dist/events')).filter(file => file.endsWith('.js'));
+
+        await handleEvents(bot,
+            eventDir,
+            bot_config,
+            database,
+            querys);
+
+        Success("Events active.");
+
+        /**
+         * Loading commands.
+         */
+        await loadCommands();
+        Success("Commands loaded.");
+
+
 
     }
 
@@ -160,7 +164,7 @@ function loadChannels() {
         throw new Error(error);
 
     };
-    
+
 })();
 
 
