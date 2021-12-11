c3d7c26acb83d012a8b54a9e2bef7445ab59ab0c
Febzey <braydent1235@gmail.com>
2021-12-10 22:00:06 -0600

    Fixed createBot.ts

--- src/createbot.ts
+++ src/createbot.ts
@@ -1,50 +1,44 @@
-import mineflayer from 'mineflayer';
+import { createBot, Bot, BotOptions} from 'mineflayer';
 import { Fail } from './util/log.js';
 import embed from './util/embed.js';
 
-const serverIsOffline = (host: string, client: any, relayChannel: string) => {
-    embed(`The server **${host}** seems to be offline 🤔.`, 'orange')
-
-
-    client.channels.cache
-        .get(relayChannel)
-        .send({
-            embeds: [{
-                color: "#ffa500",
-                description: "Should I attempt to reconnect ?"
-            }], components: [{
-                type: 1,
-                components: [
-                    {
-                        type: 2,
-                        style: 2,
-                        label: "Reconnect",
-                        custom_id: "Reconnect"
-                    },
-                    {
-                        type: 2,
-                        style: 2,
-                        label: "Reconnect in 15 minutes",
-                        custom_id: "ReconnectInTime"
-                    }
-                ]
-            }]
-        });
-}
-
-
-const loginMineflayer = (options: mineflayer.BotOptions, client: any, Channel: string) => {
+const loginMineflayer = (options: BotOptions, client: any, Channel: string) => {
     return new Promise(resolve => {
 
-        const bot: mineflayer.Bot = mineflayer.createBot(options);
-
+        const bot: Bot = createBot(options);
 
         const timeOut: ReturnType<typeof setTimeout> = setTimeout(async () => {
 
             Fail('Mineflayer bot could not connect... going to idle.');
-            serverIsOffline(options.host, client, Channel);
+            embed(`The server **${options.host}** seems to be offline 🤔.`, 'orange')
+            client.channels.cache
+            .get(Channel)
+            .send({
+                embeds: [{
+                    color: "#ffa500",
+                    description: "Should I attempt to reconnect ?"
+                }], components: [{
+                    type: 1,
+                    components: [
+                        {
+                            type: 2,
+                            style: 2,
+                            label: "Reconnect",
+                            custom_id: "Reconnect"
+                        },
+                        {
+                            type: 2,
+                            style: 2,
+                            label: "Reconnect in 15 minutes",
+                            custom_id: "ReconnectInTime"
+                        }
+                    ]
+                }]
+            });
 
-        }, 20000);
+
+
+        }, 40000);
 
         bot.once("login", () => {
             embed(`Joined ${options.host} successfully!`, "green")
