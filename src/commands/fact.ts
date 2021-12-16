3e38f0cc9c4c963bb40ee5560a765f3bf17e92e9
Febzey <braydent1235@gmail.com>
2021-12-15 19:27:42 -0600

    Added fact total and fact random

--- src/commands/fact.ts
+++ src/commands/fact.ts
@@ -8,8 +8,7 @@ export default {
             if (args.length < 2) return bot.whisper(username, "When adding a fact please also enter in a fact");
             args.shift();
             let string = args.join(' ');
-            if (/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(string)) return bot.whisper(username, "No special characters");
-            console.log(string);
+            if (/[`%^&*()\=\[\]{};':"\\|<>\/~]/.test(string)) return bot.whisper(username, "No special characters");
             database.query(querys.saveFact,
                 [username, string, Date.now()],
                 (err: unknown, res: any) => {
@@ -22,23 +21,52 @@ export default {
         }
 
         else if (/\d/.test(args[0])) {
-
             database.query(querys.searchFact,
                 [args[0]]
                 , (err: unknown, res: any[]) => {
                     if (err || !res.length) return bot.whisper(username, `The fact ID: ${args[0]} does not exist.`);
-
-                    bot.chat(`[FACT: ${res[0].id}]: ${res[0].fact}`);
-
+                    return bot.chat(`[FACT: ${res[0].id}]: ${res[0].fact}`);
                 })
+        }
+
+        else if (args[0] === 'total') { 
+
+            database.query(querys.totalFact, (err:unknown,res:any[]) => {
+
+                if (err) {
+                    bot.whisper(username, "Could not get total number of facts, sorry.");
+                    return console.error(err);
+                }
+
+
+                return bot.whisper(username, `Total facts: ${res[0].total}`)
+
+            })
 
         }
+
+        else if (args[0] === 'random') { 
+
+            database.query(querys.randomFact, (err:unknown, res:any[]) => {
+
+                if (err) {
+                    bot.whisper(username, "Could not get a random Fact, sorry.");
+                    return console.error(err);
+                }
+
+                return bot.chat(`[fact ${res[0].id}]: ${res[0].fact}`);
+
+            })
+
+
+        }
+
+
+
 
         else {
             return bot.whisper(username, "Adding fact: !fact add <fact> | Searching fact:  !fact <ID>")
         }
-
         return;
-
     }
 }
