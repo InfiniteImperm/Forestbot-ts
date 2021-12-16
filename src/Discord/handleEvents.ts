8204335ee6eebebc845252cff6f947cfa648b300
Febzey <braydent1235@gmail.com>
2021-12-15 20:57:55 -0600

    lol

--- src/Discord/handleEvents.ts
+++ src/Discord/handleEvents.ts
@@ -1,8 +1,5 @@
 export default async function handleEvents(client:any, eventDir:any) {
     for (const file of eventDir) {
-        /**
-         * Importing each event file.
-         */
         const func:object = await import(`./events/${file}`);
         const event = func['default'];
         event.once ? client.once(event.name, (...args:any) => event.execute(...args, client)) : client.on(event.name, (...args:any) => event.execute(...args, client))
--- src/functions/HandleEvents.ts
+++ src/functions/HandleEvents.ts
@@ -1,22 +1,10 @@
 import { readdir } from 'fs/promises';
 import { Bot } from 'mineflayer';
-export default async function handleEvents(bot_config, bot:Bot, querys:any, database:any) {
+export default async function handleEvents(bot_config, bot: Bot, querys: any, database: any) {
     const eventDir: string[] = (await readdir('./dist/events')).filter(file => file.endsWith('.js'));
-
     for (const file of eventDir) {
-
-        const func:any = await import(`../events/${file}`);
+        const func: any = await import(`../events/${file}`);
         const event = func.default;
-
-        if(event.once) {
-            bot.once(event.name, (...args:any) => event.execute(...args, bot, bot_config, database, querys));
-        }
-        
-        else {
-            bot.on(event.name, (...args:any) => event.execute(...args, bot, bot_config, database, querys));
-        }
-
+        event.once ? bot.once(event.name, (...args: any) => event.execute(...args, bot, bot_config, database, querys)) : bot.on(event.name, (...args: any) => event.execute(...args, bot, bot_config, database, querys))
     }
-
-
-}
+};
